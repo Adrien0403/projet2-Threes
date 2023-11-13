9c46569330b836fb2cc7b74e6f2041950ae4784d
adrientramini <adrientramini@10.3.60.43>
2023-11-13 15:30:49 +0100

    debut projet

---
+++ TP3_TRAMINI/ le jeu Thees/src/le/jeu/thees/Cell.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package le.jeu.thees;
+
+/**
+ *
+ * @author adrientramini
+ */
+public class Cell {
+    private int value;
+
+    public Cell(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
---
+++ TP3_TRAMINI/ le jeu Thees/src/le/jeu/thees/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package le.jeu.thees;
+
+/**
+ *
+ * @author adrientramini
+ */
+public class Main {
+    public static void main(String[] args) {
+        LeJeuThees threesGame = new LeJeuThees();
+        threesGame.play();
+    }
+    
+}
