122cd25e0c83a2ba2a74fe23efeee657d288a98f
adrientramini <adrientramini@10.3.60.43>
2023-11-13 17:11:38 +0100

    debut projet2

---
+++  le jeu Thees/src/le/jeu/thees/GameBoard.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package le.jeu.thees;
+
+/**
+ *
+ * @author adrientramini
+ */
+public class GameBoard {
+    private Cell[][] board;
+    private int score;
+
+    public GameBoard() {
+        // Initialisation de la grille et du score
+    }
+
+    public void spawnRandomNumber() {
+        // Logique pour faire apparaître un 1 ou 2 aléatoirement sur la grille
+    }
+
+    public void move(int direction) {
+        // Logique pour déplacer les éléments dans la direction spécifiée
+    }
+}
